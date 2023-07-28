f72cdb4fcd40825a5d95f67bc9712ac3809b5737
spectre17 <jbcboado@gmail.com>
2023-07-28 19:44:05 +0800

    fix controller issue

--- src/main/java/com/jbcboado/themePark/controller/RideController.java
+++ src/main/java/com/jbcboado/themePark/controller/RideController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/ride")
+@RestController
 @RequiredArgsConstructor
 public class RideController {
 
